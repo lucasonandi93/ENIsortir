fc32d962b494ca3defb8fba0d357cd0381ee5da8
Tanjiro356045 <maxoulemat35@gmail.com>
2023-03-07 16:50:20 +0100

    APP FIXTURES TERMINERS USER

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\DataFixtures\AppFixtures;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main_home')]
-    public function index(): Response
+    public function index(AppFixtures $appFixtures, EntityManagerInterface $entityManager): Response
     {
+       # $appFixtures->load($entityManager);
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
