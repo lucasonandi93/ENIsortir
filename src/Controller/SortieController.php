48451d3aefe35736a4147c917b284fdb69b0b66f
Valentin Robert <val35340@gmail.com>
2023-03-07 14:40:30 +0100

    modif des participants en user...

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Sortie;
+use App\Entity\User;
+use App\Entity\Ville;
 use App\Form\FiltreType;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
-use App\Repository\ParticipantRepository;
+use App\Repository\UserRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -54,35 +56,50 @@ class SortieController extends AbstractController
     }
 
     #[Route('/new', name: 'new')]
-    public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, ParticipantRepository $participantRepository): Response
+    public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, UserRepository $userRepository): Response
     {
 
         $sortie = new Sortie();
+        $ville = new Ville();
         $sortieForm = $this->createForm(SortieType::class, $sortie);
-
         $sortieForm->handleRequest($request);
 
         if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
-            if ($sortieForm->get('enregistrer')) {
-                $sortie->setEtat($etatRepository->findOneBy(['libelle' => 'Créée']));
-            } elseif ($sortieForm->get('publier')) {
-                $sortie->setEtat($etatRepository->findOneBy(['libelle' => 'Publiée']));
-            }
-            if ($sortieForm->get('inscriptionAuto')) {
-                $sortie->setOrganisateur($participantRepository->find($this->getUser()->getId()));
-            }
+            // Récupérer la ville sélectionnée dans le formulaire
+            $ville = $sortieForm->get('ville')->getData();
 
-            $sortieRepository->add($sortie, true);
+            // Définir la ville de la sortie
+            $sortie->setVille($ville);
 
-            if ($sortieForm->get('enregistrer')) {
-                $this->addFlash('success', 'Sortie créée');
-            } elseif ($sortieForm->get('publier')) {
-                $this->addFlash('success', 'Sortie publiée');
-            }
-            return $this->redirectToRoute('sortie_sortie', [
-                'id' => $sortie->getId()
-            ]);
+            // Enregistrer la sortie en base de données
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            // Rediriger l'utilisateur vers la page de détails de la sortie
+            return $this->redirectToRoute('sortie_details', ['id' => $sortie->getId()]);
         }
+
+//            if ($sortieForm->get('enregistrer')) {
+//                $sortie->setEtat($etatRepository->findOneBy(['libelle' => 'Créée']));
+//            } elseif ($sortieForm->get('publier')) {
+//                $sortie->setEtat($etatRepository->findOneBy(['libelle' => 'Publiée']));
+//            }
+//            if ($sortieForm->get('inscriptionAuto')) {
+//                $sortie->setOrganisateur($participantRepository->find($this->getUser()->getId()));
+//            }
+//
+//            $sortieRepository->add($sortie, true);
+//
+//            if ($sortieForm->get('enregistrer')) {
+//                $this->addFlash('success', 'Sortie créée');
+//            } elseif ($sortieForm->get('publier')) {
+//                $this->addFlash('success', 'Sortie publiée');
+//            }
+//            return $this->redirectToRoute('sortie_sortie', [
+//                'id' => $sortie->getId()
+//            ]);
+
 
 
         return $this->render('sortie/new.html.twig', [
