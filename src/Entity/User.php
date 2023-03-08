daf7a5d4a224f77d1cd88198525a54ae2468d085
Valentin Robert <val35340@gmail.com>
2023-03-09 00:20:26 +0100

    ajout des photos aux profils

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this big tits')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -74,7 +74,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->email;
     }
-
     public function setEmail(string $email): self
     {
         $this->email = $email;
--- src/Utils/Uploader.php
+++ src/Utils/Uploader.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class Uploader
 {
 
-    public function upload(UploadedFile $file, string $directory, string $name = ""){
+    public function upload(UploadedFile $file, string $directory, string $nom = ""){
 
         //création d'un nouveau nom
-        $newFileName = $name . "-" . uniqid() . "." . $file->guessExtension();
+        $newFileName = $nom . "-" . uniqid() . "." . $file->guessExtension();
         //copy du fichier dans le répertoire de sauvegarde en le renommant
         $file->move($directory, $newFileName);
 
